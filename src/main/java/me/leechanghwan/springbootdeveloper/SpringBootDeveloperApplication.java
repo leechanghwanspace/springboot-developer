2da5898ea9cf307ab0ca26207e48dea72482fd55
leechanghwanspace <spacecehh@naver.com>
2024-09-09 01:17:45 +0900

    [ADD] springboot-developer

---
+++ src/main/java/me/leechanghwan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,9 @@
+package me.leechanghwan.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
