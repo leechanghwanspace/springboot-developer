22a5be2cd6ed8f2a7f9bb75bdf5c6ab1c3c7eddb
leechanghwanspace <spacecehh@naver.com>
2024-09-12 02:15:22 +0900

    [LV.1] Sec.03 스프링 부트 3 이해하기 ✅

--- src/main/java/me/leechanghwan/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/me/leechanghwan/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -1,9 +1,12 @@
 package me.leechanghwan.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
     }
 }
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member (id, name) VALUES (1, '이창환')
+INSERT INTO member (id, name) VALUES (2, '창환이')
+INSERT INTO member (id, name) VALUES (3, '환창이')
