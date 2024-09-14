9bcdbaab1928efe854fa7309f4573ffda5025f20
leechanghwanspace <spacecehh@naver.com>
2024-09-15 02:18:03 +0900

    [LV.1] Sec.05 DB조작이 편해지는 ORM ✅

--- src/main/java/me/leechanghwan/springbootdeveloper/MemberRepository.java
+++ src/main/java/me/leechanghwan/springbootdeveloper/MemberRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    //JpaRepository를 상속받을 때, 엔티티 Member와 엔티티의 기본키 타임 Long을 인수로 넣어줌
 }
