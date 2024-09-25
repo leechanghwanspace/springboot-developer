1722a7da1116ad545013f6173b6f354b886fd06a
leechanghwanspace <spacecehh@naver.com>
2024-09-26 03:46:06 +0900

    [LV.2] Sec.08 SpringSecurity ✅

---
+++ src/main/java/me/leechanghwan/springbootdeveloper/dto/AddUserRequest.java
@@ -0,0 +1,11 @@
+package me.leechanghwan.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddUserRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/me/leechanghwan/springbootdeveloper/service/UserService.java
@@ -0,0 +1,24 @@
+package me.leechanghwan.springbootdeveloper.service;
+
+import lombok.RequiredArgsConstructor;
+
+import me.leechanghwan.springbootdeveloper.domain.User;
+import me.leechanghwan.springbootdeveloper.dto.AddUserRequest;
+import me.leechanghwan.springbootdeveloper.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
